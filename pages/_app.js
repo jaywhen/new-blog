30a7f80ede30ec1b0fb52c6185e38bf5abcfa42c
jaywhen <jaywhenx@gmail.com>
2024-11-06 01:26:06 +0800

    add umami tracker

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Script from "next/script";
 import "../styles.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://cloud.umami.is/script.js"
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+      ></Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }

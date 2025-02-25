8548c06818ebf014fa601b4f020cc2bb4b93577e
jaywhen <jaywhenx@gmail.com>
2025-02-25 22:33:56 +0800

    use static import image

--- components/PhotoBox.tsx
+++ components/PhotoBox.tsx
@@ -1,61 +1,68 @@
 import { ImageZoom } from 'nextra/components';
+import bj from '@/public/about/bj.jpg';
+import cq from '@/public/about/cq.jpg';
+import hkStreet from '@/public/about/hk-filming.jpg';
+import hk from '@/public/about/hk.jpg';
+import westlake from '@/public/about/hz.jpg';
+import nj00 from '@/public/about/nj-long.jpg';
+import nj01 from '@/public/about/nj.jpg';
+import sz from '@/public/about/sz.jpg';
 
 const PhotoBox: React.FC = () => {
-  const mainImgInfo = {
-    src: '/about/bj.jpg',
-    alt: 'beijing.jpeg',
+  const mainImg = {
+    src: bj,
+    alt: 'Beijing',
   };
 
-  const subImgInfo = {
-    src: '/about/hk.jpg',
-    alt: 'hongkong',
-  };
-
-  const gridImgInfos = [
+  const gridImgs = [
     {
-      src: '/about/hk.jpg',
-      alt: 'hongkong',
+      src: hk,
+      alt: 'HongKong',
     },
     {
-      src: '/about/hk-filming.jpg',
-      alt: 'filming-store-hk',
+      src: hkStreet,
+      alt: 'HongKong Street',
     },
     {
-      src: '/about/nj-long.jpg',
-      alt: 'nanjing',
+      src: nj00,
+      alt: 'Nanjing',
     },
     {
-      src: '/about/nj.jpg',
-      alt: 'nanjing',
+      src: nj01,
+      alt: 'Nanjing',
     },
     {
-      src: '/about/sz.jpg',
-      alt: 'suzhou',
+      src: sz,
+      alt: 'Suzhou',
     },
     {
-      src: '/about/hz.jpg',
-      alt: 'hangzhou',
+      src: westlake,
+      alt: 'Westlake',
     },
     {
-      src: '/about/cq.jpg',
-      alt: 'chongqing',
+      src: cq,
+      alt: 'Chongqing',
     },
   ];
 
   return (
     <div className="w-full flex flex-col gap-2">
       <ImageZoom
+        priority={true}
         className="w-full h-full object-cover m-0"
-        src={mainImgInfo.src}
-        alt={mainImgInfo.alt}
+        src={mainImg.src}
+        alt={mainImg.alt}
+        placeholder='blur'
       />
 
       <div className="w-full h-full grid grid-cols-3 gap-2 items-center">
-        {gridImgInfos.map((img, index) => (
+        {gridImgs.map((img, index) => (
           <ImageZoom
             key={index}
             className="w-full h-full object-contain m-0"
             src={img.src}
+            quality={85}
+            placeholder='blur'
             alt={img.alt}
           />
         ))}

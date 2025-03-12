eb32835d5c60aee7bde686985656f5e3301fce0a
jaywhen <jaywhenx@gmail.com>
2025-03-13 00:25:24 +0800

    add Hacker News Reader image and update blog tags; remove unused SplitText component

---
+++ components/Products.tsx
@@ -0,0 +1,54 @@
+import dss from '@/public/about/dsshare.svg';
+import hcReader from '@/public/about/hc-reader.png';
+import Image from 'next/image';
+import { useTheme } from 'nextra-theme-blog';
+
+export default function Products() {
+  const { theme } = useTheme();
+
+  const cards = [
+    {
+      icon: dss,
+      title: 'DeepSeek Share',
+      description:
+        'A chrome extension that helps you export your DeepSeek chat list',
+      link: 'https://chromewebstore.google.com/detail/deepseek-share/kdjdffoakknocbeicphnagoaidjembfd',
+      width: 180,
+    },
+    {
+      icon: hcReader,
+      title: 'Hacker News Reader',
+      description: 'Just another Hacker News clone',
+      link: 'https://hackernews-plum.vercel.app',
+      width: 40,
+    },
+  ];
+
+  const onClickCard = (card) => {
+    window.open(card.link, '_blank');
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      {cards.map((card, index) => (
+        <div
+          onClick={() => onClickCard(card)}
+          key={index}
+          className={`flex flex-col items-start cursor-pointer transition ease-in-out delay-50 duration-300 ${
+            theme === 'dark' && 'invert'
+          } bg-white lg:hover:scale-105 rounded-lg p-4 lg:hover:-translate-y-1`}
+        >
+          <Image
+            className="m-0"
+            width={card.width}
+            height={100}
+            src={card.icon}
+            alt={card.title}
+          />
+          <div className="font-semibold mt-2 text-slate-800">{card.title}</div>
+          <div className="text-gray-600 text-sm">{card.description}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
